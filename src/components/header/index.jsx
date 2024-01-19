8f1ea6561f811c169bd6065d66c8adb88eb7883a
José Victor <jose08.victor@gmail.com>
2024-01-19 20:33:21 -0300

    header added

---
+++ src/components/header/index.jsx
@@ -0,0 +1,15 @@
+import Robot from "../../assets/robot-image.jpeg";
+import "./index.scss";
+
+export const Header = () => {
+    return (
+        <header>
+            <img src={Robot} alt="Robot Image" />
+
+            <div>
+                <h1>Futuro Tecnologias</h1>
+                <p>O futuro está se aproximando rapidamente e uma nova era de inovação e ruptura digital chegou.</p>
+            </div>
+        </header>
+    )
+}
