85e3f5ef40141afbf34bf6c4648b72c269654c21
José Victor <jose08.victor@gmail.com>
2024-01-21 20:59:07 -0300

    building the main section of the project

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,16 +1,26 @@
 import "./index.scss"
 import { ButtonDefault } from "../button-default";
+
 import robot2 from "../../assets/robot-image2.jpeg";
 import robot3 from "../../assets/robot-image3.jpeg";
+
 import lamp from "../../assets/lamp.svg";
 import car from "../../assets/car.svg";
 import mind from "../../assets/mind.svg";
+
 import electricTruck from "../../assets/electric-truck.jpeg";
 import electricCarImage1 from "../../assets/electric-car-image1.jpeg";
 import electricCarImage2 from "../../assets/electric-car-image2.jpeg";
 import electricCarImage3 from "../../assets/electric-car-image3.jpeg";
 import electricCarImage4 from "../../assets/electric-car-image4.jpeg";
 import electricCarImage5 from "../../assets/electric-car-image5.jpeg";
+
+import gadgetImage1 from "../../assets/gadget-image1.jpeg";
+import gadgetImage2 from "../../assets/gadget-image2.jpeg";
+import gadgetImage3 from "../../assets/gadget-image3.jpeg";
+import gadgetImage4 from "../../assets/gadget-image4.jpeg";
+
+// import appliances from "../../assets/appliances.jpeg";
 
 export const Main = () => {
     return (
@@ -74,24 +84,78 @@ export const Main = () => {
                     <img src={electricTruck} alt="Electric Truck" />
 
                     <div>
-                        <p>TRANSPORTE</p>
+                        <p className="text-1">TRANSPORTE</p>
 
                         <h2>Caminhões elétricos tesla</h2>
 
-                        <p>O Semi da Tesla teve um começo promissor, apesar de ainda não haver informações oficiais de preços disponíveis.</p>
+                        <p className="text-2">O Semi da Tesla teve um começo promissor, apesar de ainda não haver informações oficiais de preços disponíveis.</p>
 
-                        <p>A Tesla disse que seu custo de operação para o Semi ajudará os remetentes a economizar dinheiro por milha imediatamente, e isso provavelmente ajudará a diminuir o impacto do choque do adesivo quando finalmente revelar o preço inicial. Mas, como mencionei com o piloto do Walmart, que envolverá um teste de 15 caminhões Semi nos EUA e Canadá, há valor adicional em ajudar esses grandes transportadores a cumprir suas metas verdes.</p>
+                        <p className="text-3">A Tesla disse que seu custo de operação para o Semi ajudará os remetentes a economizar dinheiro por milha imediatamente, e isso provavelmente ajudará a diminuir o impacto do choque do adesivo quando finalmente revelar o preço inicial. Mas, como mencionei com o piloto do Walmart, que envolverá um teste de 15 caminhões Semi nos EUA e Canadá, há valor adicional em ajudar esses grandes transportadores a cumprir suas metas verdes.</p>
 
                         <ButtonDefault />
                     </div>
                 </div>
 
                 <div className="cars-images">
-                    <img src={electricCarImage1} alt="Electric Car Image 1" />                
+                    <img src={electricCarImage1} alt="Electric Car Image 1" />
                     <img src={electricCarImage2} alt="Electric Car Image 2" />
                     <img src={electricCarImage3} alt="Electric Car Image 3" />
                     <img src={electricCarImage4} alt="Electric Car Image 4" />
                     <img src={electricCarImage5} alt="Electric Car Image 5" />
+                </div>
+            </section>
+
+            <section className="section-4">
+                <div className="containers">
+                    <div className="container1">
+                        <div>
+                            <h3>MÁQUINAS FAZEM MELHOR</h3>
+
+                            <p>O RPA pode ser uma atualização sobre os humanos quando a precisão e a velocidade contam - não apenas no processamento de transações, mas também em tarefas mais complexas, como relatórios regulatórios e gerenciamento de contratos.</p>
+                        </div>
+
+                        <div>
+                            <h3>SOLUÇÕES “QUICK WIN”</h3>
+
+                            <p>Essas tecnologias podem escalar rapidamente, fornecer um meio de capturar experiência operacional para uma implantação mais ampla e fornecer serviços globalmente em uma programação 24/7 em vários idiomas.</p>
+                        </div>
+
+                        <div>
+                            <h3>SERVIÇOS DE ALTO IMPACTO</h3>
+
+                            <p>A automatização de partes do processo de compensação anual permite a redistribuição de recursos de RH para focar no treinamento de gerentes e no desenvolvimento de planos de ação de engajamento.</p>
+                        </div>
+                    </div>
+
+                    <div className="container2">
+                        <h3>Os robôs estão chegando</h3>
+
+                        <p>A automação de processos robóticos (RPA) funciona no nível da interface do usuário, preenche a lacuna entre tecnologias díspares e executa processos repetitivos baseados em regras.</p>
+                    </div>
+                </div>
+            </section>
+
+            <section className="section-5">
+                <div className="gadget-images">
+                    <img src={gadgetImage1} alt="Gadget Image 1" />
+
+                    <img src={gadgetImage2} alt="Gadget Image 2" />
+
+                    <img src={gadgetImage3} alt="Gadget Image 3" />
+
+                    <img src={gadgetImage4} alt="Gadget Image 4" />
+                </div>
+
+                <div className="description">
+                    <p className="text-1">GADGETES</p>
+
+                    <h2>Gadgets do futuro</h2>
+
+                    <p className="text-2">A tecnologia está se desenvolvendo em uma velocidade enorme e você nunca sabe o que está por vir, digamos, por exemplo, em 2020.</p>
+
+                    <p className="text-3">Um dos que gostam de refletir sobre o futuro da tecnologia é o designer francês Josselin Zaigouche que decidiu projetar este gadget incrível, apelidado de Apple Black Hole (e não é porque vai cair tão bem se chegar ao mercado). O nome provavelmente foi inspirado nas inúmeras aplicações que podem ser encontradas nos aparelhos da Apple (como iPhone e iPad).</p>
+
+                    <ButtonDefault />
                 </div>
             </section>
         </main>
