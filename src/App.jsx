17d86ee3c97b5423625f533ac4653f5b1bfe37a3
José Victor <jose08.victor@gmail.com>
2024-01-22 20:55:20 -0300

    footer of the project added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Footer } from './components/footer';
 import { Header } from './components/header';
 import { Main } from './components/main';
 import './reset.scss';
@@ -7,6 +8,7 @@ function App() {
     <>
     <Header />
     <Main/>
+    <Footer/>
     </>
   )
 }
